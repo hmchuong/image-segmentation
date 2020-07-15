d0b1816a04bfa7b8238cfc67ce64fec0e45f05d0
Huỳnh Minh Chương <minhchuong.itus@gmail.com>
2020-07-15 17:00:06 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,12 @@
 
+from .hrnet import HRNet, LARGE_CONFIG, SMALL_CONFIG_V2
+from .deeplabv3plus import Deeplab_v3plus
+
+
+def create_model_cityscapes(model_name):
+  if model_name == "DeeplabV3+":
+    return Deeplab_v3plus(19)
+  elif model_name == "HRNet-W18":
+    return HRNet(19, 3, SMALL_CONFIG_V2)
+  elif model_name == "HRNet-W48":
+    return HRNet(19, 3, LARGE_CONFIG)
