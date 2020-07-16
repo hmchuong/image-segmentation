f87c25ed45dc68ec1dcb0660115e218a85f7e0a2
Huỳnh Minh Chương <minhchuong.itus@gmail.com>
2020-07-16 12:05:21 +0700

    Update preprocess_image.py

--- preprocess_image.py
+++ preprocess_image.py
@@ -7,11 +7,11 @@ std=[0.229, 0.224, 0.225]
 
 def process_image(image_path):
   global mean, std
-  image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-  image = image.astype(np.float32)[:, :, ::-1]
+  origin_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+  image = origin_image.astype(np.float32)[:, :, ::-1]
   image = image / 255.0
   image -= mean
   image /= std
   image = image.transpose((2, 0, 1))
-  return torch.from_numpy(image)
+  return origin_image, torch.from_numpy(image)
   
